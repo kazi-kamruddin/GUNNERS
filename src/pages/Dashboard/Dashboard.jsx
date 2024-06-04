2dfa4f3ffc3e4a441926191ae55799dbad07d8ae
Rakibul-rahi <rakibulislam.rahi.rir@gmail.com>
2024-06-05 04:14:32 +0600

    3rd push

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -4,7 +4,7 @@ import './dashboard.css'; // import the CSS file
 
 const FanPage = () => {
   const user = {
-    name: 'Kazi Kamruddin Ahmed',
+    name: ' Kazi  Kamruddin  Ahmed ',
     image: 'https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=1160&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // replace with the image URL
   };
 
