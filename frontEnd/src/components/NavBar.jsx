8cf540f67a942af456e551d80e7ebbf75261a055
kazi-kamruddin <kazikamruddinahmed@gmail.com>
2024-08-18 21:49:55 +0600

    remember me feature

--- frontEnd/src/components/NavBar.jsx
+++ frontEnd/src/components/NavBar.jsx
@@ -3,44 +3,56 @@ import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import './NavBar.css';
 import { useLogout } from "../hook/useLogout";
-
-
-const pages = [
-  { link: "/", text: "Home" },
-  { link: "/dashboard", text: "Dashboard" },
-  { link: "/teams", text: "Teams" },
-  { link: "/shop", text: "Shops" },
-  { link: "/news", text: "News" },
-  { link: "/login", text: "Login" },
-  { link: "/signUp", text: "SignUp" },
-];
+import { useAuthContext } from "../hook/useAuthContext";
 
 const NavBar = () => {
   let location = useLocation();
 
-  const {logout} = useLogout();
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   const handleLogout = () => {
     logout();
-  }
+  };
 
   return (
     <div className="nav-main-div">
-      <nav>
-        <ul className="nav-ul">
-          {pages.map((item, idx) => {
-            return (
-              <li key={idx} className={`nav-item ${location.pathname === item.link && "active"}`}>
-                <Link to={item.link}>{item.text}</Link>
-              </li>
-            );
-          })}
-          <div>
-              <button className="logout" onClick={handleLogout}> logout </button>
-          </div>
-        </ul>
+      <nav className="navigationBar">
+        <div className={`nav-item ${location.pathname === "/" && "active"}`}>
+          <Link to="/">Home</Link>
+        </div>
+        { user && (
+            <div className={`nav-item ${location.pathname === "/dashboard" && "active"}`}>
+              <Link to="/dashboard">Dashboard</Link>
+            </div>
+        )}
+        <div className={`nav-item ${location.pathname === "/teams" && "active"}`}>
+          <Link to="/teams">Teams</Link>
+        </div>
+        <div className={`nav-item ${location.pathname === "/shop" && "active"}`}>
+          <Link to="/shop">Shops</Link>
+        </div>
+        <div className={`nav-item ${location.pathname === "/news" && "active"}`}>
+          <Link to="/news">News</Link>
+        </div>
+        { !user && (
+            <div className={`nav-item ${location.pathname === "/login" && "active"}`}>
+              <Link to="/login">Login</Link>
+            </div>
+        )}
+        { !user && (
+            <div className={`nav-item ${location.pathname === "/signUp" && "active"}`}>
+              <Link to="/signUp">SignUp</Link>
+            </div>
+        )}
+        { user && (
+            <div className="nav-item">
+              <button className="logout" onClick={handleLogout}>Logout</button>
+            </div>
+        )}
       </nav>
     </div>
+
   );
 };
 
--- frontEnd/src/context/authContext.jsx
+++ frontEnd/src/context/authContext.jsx
@@ -1,4 +1,4 @@
-import { createContext,useReducer } from "react";
+import { createContext,useReducer,useEffect } from "react";
 
 export const authContext = createContext();
 
@@ -7,7 +7,7 @@ export const authReducer = (state, action) => {
         case 'LOGIN':
             return {user: action.payload}
         case 'LOGOUT':
-            return {user: null}
+            return {user: null} 
         default :  
             return state
     }
@@ -17,6 +17,14 @@ export const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(authReducer, {
         user : null
     });
+
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('user'));
+
+        if(user){
+            dispatch({type: 'LOGIN', payload: user});
+        }
+    }, []);
 
     console.log('authContext state: ', state);
 
