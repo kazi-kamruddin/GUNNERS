4cd3d3e1a7f6150b39d5723331a852eaadb9bca2
kazi-kamruddin <kazikamruddinahmed@gmail.com>
2024-09-04 23:33:16 +0600

    fixtures basic layout

--- frontEnd/src/components/fixtureList.jsx
+++ frontEnd/src/components/fixtureList.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import '../allCss/fixtureList.css';
+import { getCompLogo } from "../hook/getCompLogo";
+import { getLogo } from "../hook/getClubLogo";
 
 const FixtureList = ({ fixture }) => {
 
@@ -7,22 +9,29 @@ const FixtureList = ({ fixture }) => {
     <div className="single-fixture-card">
         <div className="single-fixture-info">
             <div className="info-header">
-                eikhane date, competition.
+                <div className="header-text">
+                        <div className="match-date">{fixture.date}</div>
+                        <div className="match-venue">Venue : {fixture.venue}</div>
+                </div>
+                <div className="header-image">
+                    <img src={getCompLogo(fixture.competition)} alt={`comp logo`} className="header-comp-logo"/>
+                </div>
             </div>
             <div className="info-body">
-                <div className="gg">{fixture.form}</div>
-                <div className="gg">{fixture.form}</div>
-                <div className="gg">{fixture.form}</div>
-                <div className="gg">{fixture.form}</div>
-                <div className="gg">{fixture.form}</div>
+                <div className="single-fixture-team-name">{fixture.team1}</div>
+                <div className="single-fixture-team-logo">
+                    <img src={getLogo(fixture.team1)} alt={`team logo`} className="single-fixture-teamLogo-itself"/>
+                </div>
+                <div className="single-fixture-versus">VS</div>
+                <div className="single-fixture-team-logo">
+                    <img src={getLogo(fixture.team2)} alt={`team} logo`} className="single-fixture-teamLogo-itself"/>
+                </div>
+                <div className="single-fixture-team-name">{fixture.team2}</div>
             </div>
             <div className="info-footer">
                 news button
             </div>
-            
         </div>
-        
-        <div className="divider"></div>
     </div>
   );
 };
